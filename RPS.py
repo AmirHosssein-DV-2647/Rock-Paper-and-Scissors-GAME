a9611a427294aa00dad74a938f0ae4364b639d20
AmirHosssein-DV-2647 <140967087+AmirHosssein-DV-2647@users.noreply.github.com>
2023-07-30 19:48:37 +0330

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,12 +1,10 @@
 # IMPORTS
 from colorama import Fore, init
-import subprocess
 import time
 import random
 from random import choice
 init()
 
-subprocess.call("clear", shell=True)
 # APP CODES
 while True:
     print(Fore.LIGHTCYAN_EX + "For Cancle Playing : CTRL + C")
@@ -18,25 +16,15 @@ while True:
         and System == "Paper"):
         time.sleep(1)
         print(Fore.YELLOW + "Your Choice: {} And The System Choice: {}".format(User, R))
-        print(Fore.GREEN + "You're The WINNER :)")
-        time.sleep(3)
-        subprocess.call("clear", shell = True)
+        print(Fore.GREEN + "You're The WINNER :))
 
     elif (User.lower() == "rock" and R == "Paper") or (User.lower() == "paper" and R == "Scissors") or (User.lower() == "scissors" \
         and System == "Rock"):
         time.sleep(1)
         print("Your Choice: {} And The System's Choice: {}".format(User, R))
-        print(Fore.LIGHTMAGENTA_EX + "System Is The WINNER :)")
-        time.sleep(3)
-        subprocess.call("clear", shell = True)
-        
+        print(Fore.LIGHTMAGENTA_EX + "System Is The WINNER :)")     
     elif User.lower() == R.lower():
         time.sleep(2)
         print(Fore.BLUE + "Same Choice.")
-        time.sleep(3)
-        subprocess.call("clear", shell = True)
-
     else:
         print(Fore.BLACK + "Choice A Word That Listed.")
-        time.sleep(3)
-        subprocess.call("cls", shell = True)
