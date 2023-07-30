7dad199001f4d3770493985df65442768d972fd4
AmirHosssein-DV-2647 <140967087+AmirHosssein-DV-2647@users.noreply.github.com>
2023-07-30 19:44:10 +0330

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -6,7 +6,7 @@ import random
 from random import choice
 init()
 
-subprocess.call("cls", shell=True)
+subprocess.call("clear", shell=True)
 # APP CODES
 while True:
     print(Fore.LIGHTCYAN_EX + "For Cancle Playing : CTRL + C")
@@ -20,7 +20,7 @@ while True:
         print(Fore.YELLOW + "Your Choice: {} And The System Choice: {}".format(User, R))
         print(Fore.GREEN + "You're The WINNER :)")
         time.sleep(3)
-        subprocess.call("cls", shell = True)
+        subprocess.call("clear", shell = True)
 
     elif (User.lower() == "rock" and R == "Paper") or (User.lower() == "paper" and R == "Scissors") or (User.lower() == "scissors" \
         and System == "Rock"):
@@ -28,13 +28,13 @@ while True:
         print("Your Choice: {} And The System's Choice: {}".format(User, R))
         print(Fore.LIGHTMAGENTA_EX + "System Is The WINNER :)")
         time.sleep(3)
-        subprocess.call("cls", shell = True)
+        subprocess.call("clear", shell = True)
         
     elif User.lower() == R.lower():
         time.sleep(2)
         print(Fore.BLUE + "Same Choice.")
         time.sleep(3)
-        subprocess.call("cls", shell = True)
+        subprocess.call("clear", shell = True)
 
     else:
         print(Fore.BLACK + "Choice A Word That Listed.")
